bdb18b18385803f0ef2b880adf0330a0e2746516
Adrian <adriankarlo.siangco@1.ustp.edu.ph>
2026-02-18 15:57:31 +0800

    hotfix: redirecting behavior to view on top: 0

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -3,19 +3,22 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import FloatingThemeToggle from "./FloatingThemeToggle";
 import FloatingScreenSizeTracker from "./FloatingScreenSizeTracker";
-// import FloatingContactStack from "./FloatingContactStack";
+import ScrollToTop from "./ScrollToTop";
 
 export default function PublicLayout() {
   return (
     <div className="flex flex-col min-h-screen bg-background text-text transition-colors duration-300">
+      <ScrollToTop />
+
       <Navbar />
+
       <main className="flex-1">
         <Outlet />
       </main>
+
       <Footer />
       <FloatingThemeToggle />
       <FloatingScreenSizeTracker />
-      {/* <FloatingContactStack /> */}
     </div>
   );
 }
